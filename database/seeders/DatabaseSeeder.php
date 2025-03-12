17c8d07a8a715d1d1ce377370599a2fe67d0ff1c
Maizier Barid <technohard@users.noreply.github.com>
2025-03-13 02:05:51 +0700

    setup db seeder untuk sampling
    menambahkan-
    - role seeder
    - permission seeder
    - user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            UserSeeder::class,
+        ]);
+
     }
 }
