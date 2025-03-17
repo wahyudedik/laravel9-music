6d631a7c6a97cbaadffd28d0e58a6fd8772a71b4
wahyudedik <wdedyk@gmail.com>
2025-03-17 17:33:33 +0700

    modified:   public/img/favicon.png 	modified:   public/img/icon1.png 	modified:   public/img/icon2.png 	modified:   resources/views/layouts/app.blade.php 	new file:   resources/views/layouts/landing-page.blade.php 	modified:   resources/views/welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music Cool Poll</title>
+    <title>Playlist Music</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Google Fonts -->
