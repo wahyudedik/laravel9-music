eed56918fc4df8a9f5c2630a1d446f7099b00e0f
Maizier Barid <technohard@users.noreply.github.com>
2025-04-07 03:48:38 +0700

    Create 2025_04_06_204549_add_description_and_status_to_albums_table.php

---
+++ database/migrations/2025_04_06_204549_add_description_and_status_to_albums_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('albums', function (Blueprint $table) {
+            $table->text('description')->nullable()->after('release_date');
+            $table->enum('status', ['active', 'pending', 'inactive'])->default('active')->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('albums', function (Blueprint $table) {
+            $table->dropColumn(['description', 'status']);
+        });
+    }
+};
