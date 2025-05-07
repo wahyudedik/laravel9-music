eca4882f8d7dc3e0fbded7c34bd84532a4b2bd68
Maizier Barid <technohard@users.noreply.github.com>
2025-05-07 17:37:05 +0700

    Update show.blade.php

--- resources/views/admin/songs/show.blade.php
+++ resources/views/admin/songs/show.blade.php
@@ -152,7 +152,7 @@
                             <h3 class="card-title">Song Information</h3>
                         </div>
                         <div class="card-body">
-                            <div class="datagrid">
+                            <div class="datagrid pb-4">
                                 <div class="datagrid-item">
                                     <div class="datagrid-title">Song ID</div>
                                     <div class="datagrid-content">{{ $song->id }}</div>
@@ -302,6 +302,26 @@
                                 @endforeach
 
 
+                            </div>
+
+                            <div class="row g-4 mt-1 mb-3">
+                                <div class="col">
+                                    <h5 class="fs-4">Local Zone</h5>
+                                    <div class="d-flex align-items-center">
+
+
+                                        <div class="me-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                              </svg>
+                                        </div>
+
+                                        <div>
+                                            {{ $song->local_zones }}
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                         </div>
